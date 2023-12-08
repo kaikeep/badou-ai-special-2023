0fda3390c4d5c48f7aeecdb72eb25074f3803f1b
kaikeep <143503806+kaikeep@users.noreply.github.com>
2023-12-08 21:51:18 +0800

    Add files via upload

---
+++ 147-周凯/第四周/Canny.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import cv2
+
+
+
+img = cv2.imread("lenna.png",1)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("canny",cv2.Canny(gray,200,300))
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
